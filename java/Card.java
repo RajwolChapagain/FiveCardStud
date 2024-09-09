2907039c6e2b387af0813b318facc262ff2be8f5
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-08 21:29:26 -0400

    Override compareTo function

--- java/Card.java
+++ java/Card.java
@@ -1,4 +1,4 @@
-public class Card
+public class Card implements Comparable<Card>
 {
 	private final static String[] valueMap = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 	private final static String[] suitMap = {"D", "C", "H", "S"};
@@ -25,5 +25,12 @@ public class Card
 	public void printCard()
 	{
 		System.out.print(valueMap[value] + suitMap[suit]);
+	}
+
+	@Override
+	// This function returns -1 if this card is smaller, 0 if the cards are equal, and 1 if this card is larger
+	public int compareTo(Card other)
+	{
+		return Integer.compare(this.value, other.value);
 	}
 }
