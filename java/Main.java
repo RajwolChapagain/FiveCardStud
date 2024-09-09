58bb73632a76ab7fd0acf80e448f758505845bb5
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-09 06:30:58 -0400

    Implement condition for testing

--- java/Main.java
+++ java/Main.java
@@ -1,29 +1,50 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		int NUM_HANDS = 6;
+		boolean isTesting = args.length != 0;
+		Hand[] handArray = new Hand[NUM_HANDS];
+		Deck remainingDeck = new Deck();
+
 		System.out.println("*** POKER HAND ANALYZER ***\n");
 
-		System.out.println("*** USING RANDOMIZED DECK OF CARDS ***\n");
+		if (isTesting)
+		{
 
-		Deck myDeck = new Deck();
-		System.out.println("*** Shuffled 52 card deck:");
-		myDeck.shuffle();
-		myDeck.printDeck();
-		System.out.println();
+		}
+		else
+		{
+			System.out.println("*** USING RANDOMIZED DECK OF CARDS ***\n");
+
+			Deck myDeck = new Deck();
+			System.out.println("*** Shuffled 52 card deck:");
+			myDeck.shuffle();
+			myDeck.printDeck();
+			System.out.println();
+
+			for (int i = 0; i < NUM_HANDS; i++)
+				handArray[i] = myDeck.dealHand();
+
+			remainingDeck = myDeck;
+		}
 			
 		System.out.println("*** Here are the six hands...");
 
-		for (int i = 0; i < 6; i++)
-		{
-			Hand hand = myDeck.dealHand();
-			hand.printHand();
-		}
+		for (int i = 0; i < NUM_HANDS; i++)
+			handArray[i].printHand();
+
 		System.out.println();
 
-		System.out.println("*** Here is what remains in the deck...");
-		myDeck.printDeck();
+
+		if (!isTesting)
+		{
+			System.out.println("*** Here is what remains in the deck...");
+			remainingDeck.printDeck();
+		}
 	}
 
 }
