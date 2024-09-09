e973ee8c3b333cb3bccfc639520ce512055faf35
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-08 21:15:05 -0400

    Fix casing

--- java/Card.java
+++ java/Card.java
@@ -1,7 +1,7 @@
 public class Card
 {
-	private final static String[] value_map = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	private final static String[] suit_map = {"D", "C", "H", "S"};
+	private final static String[] valueMap = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	private final static String[] suitMap = {"D", "C", "H", "S"};
 
 	private final int value;
 	private final int suit;
@@ -24,6 +24,6 @@ public class Card
 
 	public void printCard()
 	{
-		System.out.print(value_map[value] + suit_map[suit]);
+		System.out.print(valueMap[value] + suitMap[suit]);
 	}
 }
