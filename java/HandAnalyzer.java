9140e7b664e5ee1ec918bb7d92adc01284c1e7b4
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-08 21:42:50 -0400

    Create HandAnalyzer class

---
+++ java/HandAnalyzer.java
@@ -0,0 +1,10 @@
+public static class HandAnalyzer
+{
+	private static final String[] handMap = { "Royal Straight Flush", "Straight Flush", "Four of a Kind", "Full House", "Flush", "Straight", "Three of a Kind", "Two Pair", "Pair", "High Card" };
+	private static enum handType = { ROYAL_STRAIGHT_FLUSH, STRAIGHT_FLUSH, FOUR_OF_A_KIND, FULL_HOUSE, FLUSH, STRAIGHT, THREE_OF_A_KIND, TWO_PAIR, PAIR, HIGH_CARD };
+
+	private static boolean isStraight()
+	{
+
+	}
+}
