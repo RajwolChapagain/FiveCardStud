6206d23caecfa809ff412b1c5321b217b5ccf846
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-08 20:59:13 -0400

    Add basic print statements

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,28 @@
+
 public class Main
 {
 	public static void main(String[] args)
 	{
+		System.out.println("*** POKER HAND ANALYZER ***\n");
+
+		System.out.println("*** USING RANDOMIZED DECK OF CARDS ***\n");
+
 		Deck myDeck = new Deck();
+		System.out.println("*** Shuffled 52 card deck:");
+		myDeck.shuffle();
+		myDeck.printDeck();
+		System.out.println();
+			
+		System.out.println("*** Here are the six hands...");
+
+		for (int i = 0; i < 6; i++)
+		{
+			Hand hand = myDeck.dealHand();
+			hand.printHand();
+		}
+		System.out.println();
+
+		System.out.println("*** Here is what remains in the deck...");
 		myDeck.printDeck();
 	}
 
