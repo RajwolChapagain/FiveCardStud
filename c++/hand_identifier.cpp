48fce70da0f7b1e777528904163b2a530025918f
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-09-30 12:51:10 -0400

    Add method to detect Straight

--- c++/hand_identifier.cpp
+++ c++/hand_identifier.cpp
@@ -19,6 +19,32 @@ bool HandIdentifier::isRoyalStraightFlush(const vector<Card>& sortedCardList) {
     return false;
 }
 
+bool HandIdentifier::isRoyalStraight(const vector<Card>& sortedCardList) {
+    if (sortedCardList[0].getValue() == 0 && sortedCardList[1].getValue() == 9 &&
+            sortedCardList[2].getValue() == 10 && sortedCardList[3].getValue() == 11 &&
+            sortedCardList[4].getValue() == 12)
+        return true;
+
+    return false;
+}
+
+bool HandIdentifier::isStraight(const vector<Card>& sortedCardList) {
+    if (isRoyalStraight(sortedCardList))
+        return true;
+
+    int prevValue = sortedCardList[0].getValue();
+
+    for (int i = 1; i < sortedCardList.size(); i++) {
+        int currentValue = sortedCardList[i].getValue();
+        if (currentValue != prevValue + 1)
+            return false;
+
+        prevValue = currentValue;
+    }
+
+    return true;
+}
+
 bool HandIdentifier::isFlush(const vector<Card>& sortedCardList) {
     int prevSuit = sortedCardList[0].getSuit();
 
@@ -30,14 +56,4 @@ bool HandIdentifier::isFlush(const vector<Card>& sortedCardList) {
     }
 
     return true;
-}
-
-
-bool HandIdentifier::isRoyalStraight(const vector<Card>& sortedCardList) {
-    if (sortedCardList[0].getValue() == 0 && sortedCardList[1].getValue() == 9 &&
-            sortedCardList[2].getValue() == 10 && sortedCardList[3].getValue() == 11 &&
-            sortedCardList[4].getValue() == 12)
-        return true;
-
-    return false;
 }
--- c++/hand_identifier.h
+++ c++/hand_identifier.h
@@ -7,9 +7,10 @@
 class HandIdentifier {
     public:
         static void assignType(Hand& h);
-        static bool isFlush(const vector<Card>& sortedCardList);
-        static bool isRoyalStraight(const vector<Card>& sortedCardList);
         static bool isRoyalStraightFlush(const vector<Card>& sortedCardList);
+        static bool isRoyalStraight(const vector<Card>& sortedCardList);
+        static bool isStraight(const vector<Card>& sortedCardList);
+        static bool isFlush(const vector<Card>& sortedCardList);
 };
 
 #endif
