c931a759f626b981b58f6d7cf5303775991d94b9
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-10-02 17:14:00 -0400

    Implement sortTies function

--- c++/hand_sorter.cpp
+++ c++/hand_sorter.cpp
@@ -1,14 +1,43 @@
 #include <vector>
 #include <algorithm>
+#include <functional>
 #include "hand.h"
 #include "hand_sorter.h"
 
 using namespace std;
 
+vector<function<bool(const Hand&, const Hand&)>> HandSorter::comparators = {
+    compareRoyalFlush, compareRoyalFlush, compareRoyalFlush, compareRoyalFlush, compareRoyalFlush,
+    compareRoyalFlush, compareRoyalFlush, compareRoyalFlush, compareRoyalFlush, compareRoyalFlush
+};
+
 void HandSorter::sortHands(vector<Hand>& hands) {
     sortHandsByType(hands);
+    sortTies(hands);
 }
 
 void HandSorter::sortHandsByType(vector<Hand>& hands) {
     sort(hands.begin(), hands.end(), greater<Hand>());
+}
+
+void HandSorter::sortTies(vector<Hand>& hands) {
+    int startIndex = 0;
+    int lastHandType = hands[0].getType();
+
+    function<bool(const Hand&, const Hand&)> comparator;
+
+    for (int i = 0; i < hands.size(); i++) {
+	if (hands[i].getType() != lastHandType || i == hands.size() - 1) {
+	    comparator = comparators[hands[i].getType()];
+
+	    sort(hands.begin() + startIndex, hands.begin() + i - 1, comparator);
+
+	    startIndex = i;
+	    lastHandType = hands[i].getType();
+	}
+    }
+}
+
+bool HandSorter::compareRoyalFlush(const Hand& h1, const Hand& h2) {
+    return false;
 }
--- c++/hand_sorter.h
+++ c++/hand_sorter.h
@@ -2,12 +2,20 @@
 #define HAND_SORTER_H
 
 #include <vector>
+#include <functional>
 #include "hand.h"
 
+using namespace std;
+
 class HandSorter {
+    private:
+	static vector<function<bool(const Hand&, const Hand&)>> comparators;
+
     public:
         static void sortHands(vector<Hand>& hands);
         static void sortHandsByType(vector<Hand>& hands);
+        static void sortTies(vector<Hand>& hands);
+	static bool compareRoyalFlush(const Hand& h1, const Hand& h2);
 };
 
 #endif
