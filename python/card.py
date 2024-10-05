cb0a15135b8ad49031d50ac624b435f4612636e4
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-05 15:23:29 -0400

    Add ability to instantiate using string

--- python/card.py
+++ python/card.py
@@ -2,9 +2,27 @@ class Card:
     VALUE_MAP = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     SUIT_MAP = ['D', 'C', 'H', 'S']
 
-    def __init__(self, value, suit):
-        self.value = value
-        self.suit = suit
+#=============== Static methods ===============
+
+    def get_value_index(value):
+        return Card.VALUE_MAP.index(value)
+
+    def get_suit_index(suit):
+        return Card.SUIT_MAP.index(suit) 
+
+#=============== Instance methods ===============
+
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.value = args[0]
+            self.suit = args[1]
+        elif len(args) == 1:
+            string = args[0].strip()
+            value_string = string[:-1]
+            suit_string = string[-1]
+            
+            self.value = Card.get_value_index(value_string)
+            self.suit = Card.get_suit_index(suit_string)
 
     def __lt__(self, other):
         return self.get_value() < other.get_value()
@@ -24,8 +42,3 @@ class Card:
     def get_suit(self):
         return self.suit
 
-    def get_value_index(value):
-        return Card.VALUE_MAP.index(value)
-
-    def get_suit_index(suit):
-        return Card.SUIT_MAP.index(suit) 
