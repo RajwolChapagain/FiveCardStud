c1f350e799dfb8d3c1747f2c81c86e734f1712c6
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-05 13:26:16 -0400

    Add deck-dealing, hand-printing, and remaining-deck-printing functions

--- python/main.py
+++ python/main.py
@@ -1,8 +1,11 @@
 import sys
+from card import Card
 from deck import Deck
+from hand import Hand
 
 def main():
     is_testing = len(sys.argv) > 1
+    hands = []
 
     print('*** P O K E R   H A N D   A N A L Y Z E R ***\n\n')
 
@@ -11,6 +14,9 @@ def main():
     else:
         deck = Deck()
         print_deck(deck)
+        deal_from_deck(hands, deck)
+        print_hands(hands)
+        print_remaining_deck(deck)
 
 #=============== Non-testing functions ===============
 
@@ -19,6 +25,27 @@ def print_deck(deck):
 
     print ('*** Shuffled 52 card deck:')
     print(deck)
+
+def deal_from_deck(hands, deck):
+    NUM_HANDS  = 6
+
+    for i in range(NUM_HANDS):
+        hands.append(Hand())
+
+    for i in range(Hand.HAND_SIZE):
+        for j in range(NUM_HANDS):
+            hands[j].add_card(deck.deal_card())
+
+def print_hands(hands):
+    print('*** Here are the six hands...')
+
+    for hand in hands:
+        print(hand)
+    print()
+
+def print_remaining_deck(deck):
+    print('*** Here is what remains in the deck...')
+    print(repr(deck))
 
 if __name__ == '__main__':
     main()
