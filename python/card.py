3ad33a6f75a27f0e5a7abfefe216237762ad12a1
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-05 12:08:28 -0400

    Replace double with single quotes

--- python/card.py
+++ python/card.py
@@ -1,6 +1,6 @@
 class Card:
-    VALUE_MAP = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    SUIT_MAP = ["D", "C", "H", "S"]
+    VALUE_MAP = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    SUIT_MAP = ['D', 'C', 'H', 'S']
 
     def __init__(self, value, suit):
         self.value = value
@@ -12,11 +12,11 @@ class Card:
     def __str__(self):
         PRINT_WIDTH = 4
         card_string = Card.VALUE_MAP[self.get_value()] + Card.SUIT_MAP[self.get_suit()]
-        return f"{card_string : <{PRINT_WIDTH}}"
+        return f'{card_string : <{PRINT_WIDTH}}'
 
     def __repr__(self):
         class_name = self.__class__.__name__
-        return f"{class_name}(value={self.get_value()}, suit={self.get_suit()})"
+        return f'{class_name}(value={self.get_value()}, suit={self.get_suit()})'
 
     def get_value(self):
         return self.value
