8a9b895db84842debcb44126ed484b75363905fe
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-10 15:11:31 -0400

    Add subroutine to sort ties and Royal Straight Flush comparator

--- fortran/hand_sorter.f90
+++ fortran/hand_sorter.f90
@@ -9,6 +9,7 @@ contains
         type(hand) :: hands(0:5)
 
         call sort_by_type(hands)
+        call sort_ties(hands)
     end subroutine sort_hands
 
     subroutine sort_by_type(hands)
@@ -17,6 +18,37 @@ contains
         call sort(hands, compare_hands_by_type)
     end subroutine sort_by_type
 
+    subroutine sort_ties(hands)
+        type(hand) :: hands(0:5)
+        integer :: start_index, last_type, i
+
+        start_index = 0
+        last_type = hands(0)%hand_type
+
+        do i = 0, 5
+            if (hands(i)%hand_type /= last_type) then
+                call sort_subarray(hands(start_index: i-1))
+
+                start_index = i
+                last_type = hands(i)%hand_type
+            else if (i == size(hands) - 1) then
+                call sort_subarray(hands(start_index:))
+            end if
+        end do
+    end subroutine sort_ties
+
+    subroutine sort_subarray(hands)
+        type(hand) :: hands(:)
+        integer :: hand_type
+
+        hand_type = hands(1)%hand_type
+
+        if (hand_type == 9) then
+            call sort(hands, compare_royal_flush)
+        end if
+
+    end subroutine sort_subarray
+    
     subroutine sort(array, cmp_fn)
         type(hand) :: array(:)
         type(hand) :: temp
@@ -39,7 +71,7 @@ contains
     ! All return true if hand1 is weaker
 
     logical function compare_hands_by_type(hand1, hand2) result(b)
-        type(hand) :: hand1, hand2
+        type(hand), intent(in) :: hand1, hand2
 
         b = .false.
 
@@ -48,5 +80,19 @@ contains
         end if
     end function compare_hands_by_type
 
+
+    logical function compare_royal_flush(h1, h2) result(b)
+        type(hand), intent(in) :: h1, h2
+        type(card) :: l1(5), l2(5)
+
+        b = .true.
+
+        l1 = h1%get_sorted_cards()
+        l2 = h2%get_sorted_cards()
+
+        if (l1(1)%get_suit() > l2(1)%get_suit()) then
+            b = .false.
+         end if
+    end function compare_royal_flush
 
 end module
