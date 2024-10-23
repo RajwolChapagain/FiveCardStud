c68f7654200efb3deac719b4ee3b45001dc76ecc
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-23 18:37:35 -0400

    Deal hands from deck and print remaining deck

--- julia/main.jl
+++ julia/main.jl
@@ -1,7 +1,10 @@
 include("card.jl")
+include("hand.jl")
 
 println("*** P O K E R   H A N D   A N A L Y Z E R ***\n\n")
 
+# =============== Non-testing functions ===============
+#
 function init_deck(deck :: Vector{Card})
     for i in eachindex(deck) 
         deck[i] = Card(((i - 1) % 13) + 1, ceil(i / 13))
@@ -28,7 +31,41 @@ function print_deck(deck :: Vector{Card})
             println()
         end
     end
+    println()
 end
+
+function deal_from_deck(hands :: Vector{Hand}, deck :: Vector{Card})
+    for i in 1:6
+        for hand in hands
+            add_card(hand, popfirst!(deck))
+        end
+    end
+end
+
+function print_remaining_deck(deck :: Vector{Card})
+    println("*** Here is what remains in the deck...")
+    for card in deck
+        print(card)
+    end
+    println()
+end
+
+# =============== Testing functions ===============
+
+# =============== Common functions ===============
+
+function print_hands(hands :: Vector{Hand})
+    println("*** Here are the six hands...")
+
+    for hand in hands
+        print(hand)
+        println()
+    end
+    println()
+end
+
+# =============== Logic ===============
+hands = [Hand() for _ in 1:6]
 
 if length(ARGS) == 1
     println("*** USING TEST DECK ***\n")
@@ -36,4 +73,7 @@ else
     deck = Vector{Card}(undef, 52)
     init_deck(deck)
     print_deck(deck)
+    deal_from_deck(hands, deck)
+    print_hands(hands)
+    print_remaining_deck(deck)
 end
