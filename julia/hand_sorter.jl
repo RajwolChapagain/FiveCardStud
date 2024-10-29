78ae2fb4e9048a523f9488c5c0181e5a8db6d2d9
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-29 18:11:49 -0400

    Add hand_sorter with ability to sort Royal Straight Flush subarray

--- julia/hand_sorter.jl
+++ julia/hand_sorter.jl
@@ -1,3 +1,32 @@
 function sort_hands(hands::Vector{Hand})
     sort!(hands, by = hand -> hand.type, rev = true)
+
+    start_index = 1
+    last_type = hands[1].type
+
+   
+    for (i, hand) in enumerate(hands)
+        if hand.type != last_type
+            hands[start_index:i-1] = sort(hands[start_index:i-1], lt=comparators[last_type], rev=true)
+
+            start_index = i
+            last_type = hand.type
+        elseif i == length(hands)
+            hands[start_index:length(hands)] = sort(hands[start_index:length(hands)], lt=comparators[last_type], rev=true)
+        end
+    end
+    
 end
+
+# =============== Comparators ===============
+# All return true if first hand is weaker
+
+function compare_royal_flush(h1, h2)
+    if get_sorted_cards(h1)[1].suit < get_sorted_cards(h2)[1].suit
+        return true
+    end
+
+    return false
+end
+
+comparators = [compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush] 
