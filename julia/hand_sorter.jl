4615bcf5a12d2d8e7ff0b7a482d6ed87aad1d0c5
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-10-29 18:34:18 -0400

    Add helper method to compare highest card

--- julia/hand_sorter.jl
+++ julia/hand_sorter.jl
@@ -30,3 +30,58 @@ function compare_royal_flush(h1, h2)
 end
 
 comparators = [compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush] 
+
+
+# =============== Helpers ===============
+
+# Returns:
+#  1 if l1 is weaker
+# -1 if l2 is weaker
+#  0 if both are tied
+function compare_highest_card(l1, l2)
+    value_list1 = []
+    value_list2 = []
+
+    for (card1, card2) in zip(l1, l2)
+        if card1.value == 1
+            if length(l1) == HAND_SIZE
+                if is_straight(l1) && !is_royal_straight(l1)
+                    push!(value_list1, 1)
+                else
+                    push!(value_list1, 14)
+                end
+            else
+                push!(value_list1, 14)
+            end
+        else
+            push!(value_list1, card1.value)
+        end
+
+        if card2.value == 1
+            if length(l2) == HAND_SIZE
+                if is_straight(l2) && !is_royal_straight(l2)
+                    push!(value_list2, 1)
+                else
+                    push!(value_list2, 14)
+                end
+            else
+                push!(value_list2, 14)
+            end
+        else
+            push!(value_list2, card2.value)
+        end
+    end
+
+    sort!(value_list1, rev=true)
+    sort!(value_list2, rev=true)
+
+    for (value1, value2) in zip(value_list1, value_list2)
+        if value1 > value2
+            return -1
+        elseif value1 < value2
+            return 1
+        end
+    end
+
+    return 0
+end
