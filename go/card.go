32552541f85a42c725835005e6fce8e8b33a29e4
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-03 12:21:14 -0500

    Create card struct and add method to construct it using string

---
+++ go/card.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "strings"
+    "fmt"
+    "os"
+)
+
+var VALUE_MAP = [13]string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+var SUIT_MAP = [4]string{"D", "C", "H", "S"}
+
+type Card struct {
+    value, suit int 
+}
+
+func CardFromString(cardString string) Card {
+    cardString = strings.TrimSpace(cardString) 
+   
+    valueString := cardString[:len(cardString) - 1]
+    suitString := cardString[len(cardString)-1:]
+
+    valueIndex := -1
+    suitIndex := -1
+
+    for i, value := range VALUE_MAP {
+        if value == valueString {
+            valueIndex = i
+            break
+        }
+    }
+
+    for i, suit := range SUIT_MAP {
+        if suit == suitString {
+            suitIndex = i
+            break
+        }
+    }
+
+    if valueIndex == -1 {
+        fmt.Println("Error: Can't create card with value", valueString)
+        os.Exit(1)
+    }
+
+    if suitIndex == -1 {
+        fmt.Println("Error: Can't create card with suit", suitString)
+        os.Exit(1)
+    }
+    
+    return Card{valueIndex, suitIndex} 
+}
