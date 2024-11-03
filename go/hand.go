25b62cd937376891f56dac082d076a630ce9df17
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-03 15:57:04 -0500

    Create hand struct with a method to add card

---
+++ go/hand.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+const HAND_SIZE = 5
+var HAND_MAP = [10]string{"High Card", "Pair", "Two Pair", "Three of a Kind", "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush", "Royal Straight Flush"}
+
+type Hand struct {
+    cards []Card 
+    handType int
+}
+
+func (h *Hand) AddCard(c Card) {
+    if len(h.cards) != HAND_SIZE {
+        (*h).cards = append((*h).cards, c)
+    } else {
+        fmt.Println("Error: Hand can't contain more than 5 cards")
+        os.Exit(1)
+    }
+}
