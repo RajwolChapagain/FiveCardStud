8bf8cbed34049d992d02a2a38266176b8df35562
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-10 08:33:35 -0500

    Construct from string

--- perl/Card.pm
+++ perl/Card.pm
@@ -7,15 +7,52 @@ our @VALUE_MAP = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "
 our @SUIT_MAP = ("D", "C", "H", "S");
 
 sub new {
-    my $class = shift;
-    my ($value, $suit) = @_;
+    my ($class, @args) = @_;
 
-    my $self = bless {
-        value => $value,
-        suit => $suit
-    }, $class;
+    if (@args == 2) {
+        my ($value, $suit) = @args;
 
-    return $self;
+        my $self = bless {
+            value => $value,
+            suit => $suit
+        }, $class;
+
+        return $self;
+    } elsif (@args == 1) {              # If a string is passed in
+        my $string = $args[0];
+        my ($value_string, $suit_string);
+        my ($value, $suit);
+
+        $string =~ s/^\s+//;            # Remove preceding spaces
+
+        if (length($string) == 3) {     # If it's a 10 card
+            $value_string = substr($string, 0, 2); 
+            $suit_string = substr($string, -1, 1); 
+
+        } elsif (length($string) == 2) {
+            $value_string = substr($string, 0, 1); 
+            $suit_string = substr($string, -1, 1); 
+
+        }
+
+        $value = get_value_index($value_string);
+        $suit = get_suit_index($suit_string);
+
+        my $self = bless {
+            value => $value,
+            suit => $suit
+        }, $class;
+
+        return $self;
+         
+    } elsif (@args == 0) {              # By default
+        my $self = bless {
+            value => 0,
+            suit => 0
+        }, $class;
+
+        return $self;
+    }
 }
 
 sub to_string {
@@ -34,6 +71,30 @@ sub get_suit {
     my $self = shift;
 
     return $self->{suit};
+}
+
+sub get_value_index {
+    my $value_string = shift; 
+    
+    for my $i (0..$#VALUE_MAP) {
+        if ($VALUE_MAP[$i] eq $value_string) {
+            return $i;
+        }
+    }
+
+    return -1;
+}
+
+sub get_suit_index {
+    my $suit_string = shift; 
+    
+    for my $i (0..$#SUIT_MAP) {
+        if ($SUIT_MAP[$i] eq $suit_string) {
+            return $i;
+        }
+    }
+
+    return -1;
 }
 
 return 1;
