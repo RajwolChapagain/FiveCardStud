60393c29b4bb6c5a8cf866db573e5b5946606ac4
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-10 15:13:03 -0500

    Create HandIdentifier that detects Royal Flush and Flush

---
+++ perl/HandIdentifier.pm
@@ -0,0 +1,54 @@
+package HandIdentifier;
+
+use strict;
+use warnings;
+
+sub assign_type {
+    my $hand = shift;
+
+    my @cards = $hand->get_sorted_cards;
+
+    if (is_royal_straight_flush(@cards)) {
+        $hand->set_type(9);
+    } elsif (is_flush(@cards)) {
+        $hand->set_type(5);
+    } else {
+        $hand->set_type(0);
+    }
+}
+
+sub is_royal_straight_flush {
+    my @cards = @_;
+
+    if (is_royal_straight(@cards) and is_flush(@cards)) {
+        return 1;
+    }
+
+   return 0;
+}
+
+sub is_royal_straight {
+    my @cards = @_;
+
+    if ($cards[0]->get_value == 0 and $cards[1]->get_value == 9 and $cards[2]->get_value == 10 and $cards[3]->get_value == 11 and $cards[4]->get_value == 12) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+sub is_flush {
+    my @cards = @_;
+
+    my $prev_suit = $cards[0]->get_suit;
+
+    foreach my $card (@cards) {
+        if ($card->get_suit != $prev_suit) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+return 1;
