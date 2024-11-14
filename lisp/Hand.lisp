40f8344275277af663485e3ef4f291e987f4785d
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-14 08:08:26 -0500

    Add to-string method

--- lisp/Hand.lisp
+++ lisp/Hand.lisp
@@ -1,6 +1,7 @@
 (defparameter *hand-map* (list "High Card" "Pair" "Two Pair" "Three of a Kind" "Straight" "Flush" "Full House" "Four of a Kind" "Straight Flush" "Royal Straight Flush"))
 
 (defgeneric add-card(hand card))
+(defgeneric to-string(hand))
 
 (defclass hand()
   ((cards :accessor hand-cards
@@ -12,3 +13,9 @@
 
 (defmethod add-card(hand card)
     (setf (hand-cards hand) (nconc (hand-cards hand) (list card))))
+
+(defmethod to-string(hand)
+  (let ((result ""))
+    (dolist (card (hand-cards hand))
+      (setq result (concatenate 'string result (format nil "~4a" (to-string card)))))
+  result))
