81bda5ab31796c1b37f6086548f5a389d2d0ccad
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-14 15:58:19 -0500

    Detect Royal Straight Flush and Flush

---
+++ lisp/HandIdentifier.lisp
@@ -0,0 +1,23 @@
+(defun is-royal-straight-flush (cards)
+  (and (is-royal-straight cards) (is-flush cards)))
+
+(defun is-royal-straight (cards)
+  (and (= (card-value (first cards)) 0)
+       (= (card-value (second cards)) 9)
+       (= (card-value (third cards)) 10)
+       (= (card-value (fourth cards)) 11)
+       (= (card-value (fifth cards)) 12)))
+
+(defun is-flush (cards)
+  (let ((prev-suit (card-suit (first cards))))
+    (dolist (card cards)
+      (if (not (= (card-suit card) prev-suit))
+        (return-from is-flush nil))))
+  T)
+
+(defun assign-type (hand)
+  (let ((cards (get-sorted-cards hand)))
+    (cond 
+      ((is-royal-straight-flush cards) (setf (hand-type hand) 9))
+      ((is-flush cards) (setf (hand-type hand) 5))
+      ((setf (hand-type hand) 0)))))
