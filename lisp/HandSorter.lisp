415ba09a2ab788b9d273b270fb83246aa5a102bd
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-14 20:26:18 -0500

    Compare highest card

--- lisp/HandSorter.lisp
+++ lisp/HandSorter.lisp
@@ -1,4 +1,7 @@
 ; =============== Comparators ===============
+; All return:
+; T if h1 > h2
+; NIL if h1 < h2
 
 (defun compare-royal-flush (h1 h2)
   (> (card-suit (first (get-sorted-cards h1))) (card-suit (first (get-sorted-cards h2)))))
@@ -32,3 +35,43 @@
 (defun sort-subarray (hands)
   (let ((hand-type (hand-type (first hands))))
     (sort hands (nth hand-type comparators))))
+
+; =============== Helpers ===============
+
+; Returns:
+;  1 if l1 < l2
+; -1 if l1 > l2
+;  0 if l1 = l2
+(defun compare-highest-card (l1 l2)
+  (let ((value-list1 (list))
+        (value-list2 (list)))
+
+    (loop for i from 0 to (- (length l1) 1) do
+          (progn
+            (setf value-list1 (nconc value-list1 (list (card-value (nth i l1)))))
+            (if (= (card-value (nth i l1)) 0)
+              (progn
+                (setf (nth i value-list1) 13)
+                (if (= (length l1) *hand-size*)
+                  (progn
+                    (if (and (is-straight l1) (not (is-royal-straight l1)))
+                      (setf (nth i value-list1) 0))))))
+
+            (setf value-list2 (nconc value-list2 (list (card-value (nth i l2)))))
+            (if (= (card-value (nth i l2)) 0)
+              (progn
+                (setf (nth i value-list2) 13)
+                (if (= (length l2) *hand-size*)
+                  (progn
+                    (if (and (is-straight l2) (not (is-royal-straight l2)))
+                      (setf (nth i value-list2) 0))))))))
+
+    (setf value-list1 (sort value-list1 #'<))
+    (setf value-list2 (sort value-list2 #'<))
+
+    (loop for i from 0 to (- (length value-list1) 1) do
+          (cond
+            ((< (nth i value-list1) (nth i value-list2)) (return-from compare-highest-card 1))
+            ((> (nth i value-list1) (nth i value-list2)) (return-from compare-highest-card -1))))
+
+    (return-from compare-highest-card 0)))
