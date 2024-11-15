9958701a2c056a8a6cf60ee202040d89436fa03d
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-14 19:30:10 -0500

    Sort Royal Flush

--- lisp/HandSorter.lisp
+++ lisp/HandSorter.lisp
@@ -1,5 +1,34 @@
+; =============== Comparators ===============
+
+(defun compare-royal-flush (h1 h2)
+  (> (card-suit (first (get-sorted-cards h1))) (card-suit (first (get-sorted-cards h2)))))
+
+
+; =============== Main ===============
+
+(defparameter comparators (list #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush #'compare-royal-flush))
+
 (defun sort-hands (hands)
-  (setf hands (sort-by-type hands)))
+  (setf hands (sort-by-type hands))
+  (setf hands (sort-ties hands)))
 
 (defun sort-by-type (hands)
   (sort hands #'hand>))
+
+(defun sort-ties (hands)
+  (let ((start-index 0)
+        (last-type (hand-type (first hands))))
+
+    (loop for i from 0 to (- (length hands) 1) do
+      (cond
+        ((/= (hand-type (nth i hands)) last-type) 
+         (progn
+           (setf (subseq hands start-index i) (sort-subarray (subseq hands start-index i)))
+           (setq start-index i)
+           (setf last-type (hand-type (nth i hands)))))
+        ((= i (- (length hands) 1))
+           (setf (subseq hands start-index) (sort-subarray (subseq hands start-index))))))))
+
+(defun sort-subarray (hands)
+  (let ((hand-type (hand-type (first hands))))
+    (sort hands (nth hand-type comparators))))
