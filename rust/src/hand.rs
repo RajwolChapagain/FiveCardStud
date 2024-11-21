bf34cb5e02608617fff5c2df14c58791161a60fe
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-21 11:51:20 -0500

    Create hand module

---
+++ rust/src/hand.rs
@@ -0,0 +1,46 @@
+use itertools::Itertools;
+use crate::card::Card;
+
+pub const HAND_SIZE: usize = 5;
+pub const HAND_MAP: [&str; 10] = ["High Card", "Pair", "Two Pair", "Three of a Kind", "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush", "Royal Straight Flush"];
+
+pub struct Hand {
+    hand_type: usize,
+    cards: Vec<Card>,
+}
+
+impl Hand {
+    pub fn new() -> Self {
+        Self {hand_type: 0, cards: Vec::new() }
+    }
+
+    pub fn new_from_str(line: &str) -> Self {
+        let split: Vec<&str> = line.split(',').collect();        
+
+        let mut hand = Self::new();
+
+        for token in split {
+            hand.add_card(Card::new_from_str(token));
+        }
+
+        hand
+    }
+
+    pub fn add_card(&mut self, card: Card) {
+        self.cards.push(card);
+    }
+
+    pub fn get_sorted_cards(&self) -> Vec<Card> {
+        self.cards.iter().sorted().cloned().collect()
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut result = String::new();
+
+        for card in &self.cards {
+            result.push_str(&(format!("{:<4}", &card.to_string())));
+        }
+
+        result
+    }
+}
