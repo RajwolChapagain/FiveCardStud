09bb77a2b32f5328a3142623d68d7cd89d1dfc60
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-21 17:07:06 -0500

    Sort Straight Flush

--- rust/src/hand_sorter.rs
+++ rust/src/hand_sorter.rs
@@ -3,7 +3,7 @@ use crate::hand;
 use crate::hand::Hand;
 use crate::hand_identifier;
 
-const COMPARATORS: [fn(&Hand, &Hand) -> bool; 10] = [compare_royal_flush,compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush];
+const COMPARATORS: [fn(&Hand, &Hand) -> bool; 10] = [compare_royal_flush,compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_royal_flush, compare_straight_flush, compare_royal_flush];
 
 pub fn sort_hands(hands: &mut Vec<Hand>) {
     sort_by_type(hands);
@@ -61,11 +61,24 @@ fn compare_royal_flush(h1: &Hand, h2: &Hand) -> bool {
     let l1 = h1.get_sorted_cards();
     let l2 = h2.get_sorted_cards();
 
-    compare_highest_card(&l1, &l2);
-
     l1[0].get_suit() < l2[0].get_suit()
 }
 
+fn compare_straight_flush(h1: &Hand, h2: &Hand) -> bool {
+    let l1 = h1.get_sorted_cards();
+    let l2 = h2.get_sorted_cards();
+
+    let highest_card_comparison = compare_highest_card(&l1, &l2);
+
+    if highest_card_comparison == 1 {
+        return true;
+    }
+    else if highest_card_comparison == -1 {
+        return false;
+    }
+
+    l1[0].get_suit() < l2[0].get_suit()
+}
 // =============== Helpers ===============
 
 // Returns:
